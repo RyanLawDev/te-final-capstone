aabc62ce38658be5c21e365de2341d799ced9d56
josh brown <jbro463@gmail.com>
2024-08-05 12:33:23 -0400

    push capstone 8/5 12:33

---
+++ vue/src/components/SearchBands.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+
+        <input type="text" name="bandName" id="bandName" placeholder="Search for bands .." v-model="nameFilter">
+        <div>
+            <band-item v-bind:band=band v-for="band in filterBands" v-bind:key="band.id"> </band-item>
+        </div>
+
+    </div>
+</template>
+  
+<script>
+import bandItem from './BandItem.vue';
+
+export default {
+    components:{
+        bandItem
+    },
+    data() {
+        return {
+            nameFilter: '',
+            bands: [
+                {
+                    id: 1001,
+                    name: 'echo',
+                    album: 'wow guys',
+                    members: ['josh', 'kari', 'ryan', 'maddie', 'eric'],
+                    img_url: ['stuff'],
+                    genre: ['music genre', 'rock', 'hard'],
+                    socials: ['insta', 'twitter', 'facebook']
+                },
+                {
+                    id: 1002,
+                    name: 'brian',
+                    album: 'wow guys',
+                    members: ['josh', 'kari', 'ryan', 'maddie', 'eric'],
+                    img_url: ['stuff'],
+                    genre: ['music genre', 'rock', 'hard'],
+                    socials: ['insta', 'twitter', 'facebook']
+                },
+                {
+                    id: 1003,
+                    name: 'echo and the bunnymen',
+                    album: 'wow guys',
+                    members: ['josh', 'kari', 'ryan', 'maddie', 'eric'],
+                    img_url: ['stuff'],
+                    genre: ['music genre', 'rock', 'hard'],
+                    socials: ['insta', 'twitter', 'facebook']
+                }
+            ]
+        }
+    },
+    computed: {
+        filterBands() {
+
+            return this.bands.filter((band) => {
+                return this.nameFilter == '' ? true : band.name.includes(this.nameFilter);
+            });
+        }
+
+    }
+
+};
+</script>
+  
