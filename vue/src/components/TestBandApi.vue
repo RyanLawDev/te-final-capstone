564e771096e05a7af8c71f8c3e17a0d028b67a5e
Ryan Law <ryanhlaw13@gmail.com>
2024-08-05 12:34:40 -0400

    pull

---
+++ vue/src/components/TestBandApi.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button  v-on:click="retrieveBands" type="button">get bands</button>
+    <p>{{ bands }}</p>
+
+  </div>
+</template>
+
+<script>
+import testBandApiService from '../services/TestBandApiService.js'
+
+export default {
+    
+
+    data() {
+        return {
+            bands: {}
+
+        }
+        
+
+    },
+
+    methods: {
+        retrieveBands() {
+            this.bands = testBandApiService.getBands();
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,10 +2,15 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+    <TestBandApi/>
   </div>
 </template>
 
 <script>
+import TestBandApi from '../components/TestBandApi.vue';
 export default {
+  components: {
+    TestBandApi
+  }
 };
 </script>
