e42afff16e009545e16db457c14ca345771380be
Ryan Law <ryanhlaw13@gmail.com>
2024-08-05 13:18:11 -0400

    pushing and pulling

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,30 +2,17 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
-<<<<<<< HEAD
-    <TestBandApi/>
-=======
     <home-page />
->>>>>>> 874c9714cc9fcecca9440efda159a3e9333b3714
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import TestBandApi from '../components/TestBandApi.vue';
-export default {
-  components: {
-    TestBandApi
-=======
+
 import HomePage from '../components/HomePage.vue'
 
 export default {
   components:{
     HomePage
-  },
-  data(){
-  
->>>>>>> 874c9714cc9fcecca9440efda159a3e9333b3714
   }
 };
 </script>
