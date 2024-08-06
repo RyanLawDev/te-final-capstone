2c9d602f4e8c8d64d28a1390435f1b4728e365d5
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-08-06 18:55:58 -0400

    Spotify API returning access token

--- vue/src/services/MusicSearchService.js
+++ vue/src/services/MusicSearchService.js
@@ -2,49 +2,63 @@ import axios from 'axios';
 
 
 export default {
-     async getToken(client_id, client_secret) {
-        const item = encodeURIComponent(`'grant_type' : 'client_credentials'`)
+    //  async getToken(client_id, client_secret) {
+    //     const item = encodeURIComponent(`'grant_type' : 'client_credentials'`)
+    //     const response = await fetch('https://accounts.spotify.com/api/token', {
+    //         method: 'POST',
+    //         // body: new URLSearchParams({
+    //         //     'grant_type': 'client_credentials',
+    //         // }),
+    //         body: item.toString,
+    //         headers: {
+    //             'Content-Type': 'application/x-www-form-urlencoded',
+    //             'Authorization': 'Basic ' + (client_id + ':' + client_secret),
+    //         },
+    //     });
+
+    //     // fetch('https://accounts.spotify.com/api/token', {
+    //     //     method: 'POST',
+    //     //     body: new URLSearchParams({
+    //     //         'grant_type': 'client_credentials',
+    //     //     }),
+    //     //     headers: {
+    //     //         'Content-Type': 'application/x-www-form-urlencoded',
+    //     //         'Authorization': 'Basic ' + (client_id + ':' + client_secret),
+    //     //     },
+    //     // }).then(response=> {
+    //     //     const res = response.json()
+    //     //     console.log(res)
+    //     //     return res
+    //     // }).catch(error => {
+    //     //     console.log(error)
+    //     // });
+
+
+    //     return await response.json();
+    // },
+    async getToken(client_id, client_secret) {
+        let buf = (client_id + ':' + client_secret).toString('base64');
         const response = await fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
-            // body: new URLSearchParams({
-            //     'grant_type': 'client_credentials',
-            // }),
-            body: item.toString,
+            body: new URLSearchParams({
+                'grant_type': 'client_credentials',
+            }),
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + (client_id + ':' + client_secret),
+                Authorization: 'Basic ' + ('NmRmYmY4N2Q0YjY3NGI0N2JiMmE5MjdjZTMxYmY2NmE6Nzk4NGNiMDJlOGI0NGM2Yzk1ODJlNzdkM2EzNTA4ODI='),
             },
         });
-
-        // fetch('https://accounts.spotify.com/api/token', {
-        //     method: 'POST',
-        //     body: new URLSearchParams({
-        //         'grant_type': 'client_credentials',
-        //     }),
-        //     headers: {
-        //         'Content-Type': 'application/x-www-form-urlencoded',
-        //         'Authorization': 'Basic ' + (client_id + ':' + client_secret),
-        //     },
-        // }).then(response=> {
-        //     const res = response.json()
-        //     console.log(res)
-        //     return res
-        // }).catch(error => {
-        //     console.log(error)
-        // });
-
-
         return await response.json();
     },
     async getTrackInfo(access_token) {
-        const response = await fetch("https://api.spotify.com/v1/tracks/4cOdK2wGLETKBW3PvgPWqT", {
-            method: 'GET',
-            headers: { 'Authorization': 'Bearer ' + access_token },
-        });
+    const response = await fetch("https://api.spotify.com/v1/tracks/4cOdK2wGLETKBW3PvgPWqT", {
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + access_token },
+    });
 
-        return await response.json();
-    },
-    getArtistData() {
-        return axios.get('https://musicbrainz.org/ws/2/artist/?query=ac%5C%2Fdc');
-    }
+    return await response.json();
+},
+getArtistData() {
+    return axios.get('https://musicbrainz.org/ws/2/artist/?query=ac%5C%2Fdc');
+}
 }
