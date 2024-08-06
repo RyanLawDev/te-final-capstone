ba887587c93c637b29be19f04019e0351f887a63
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-08-06 11:40:13 -0400

    Fixed

--- java/src/main/java/com/techelevator/dao/FollowDao.java
+++ java/src/main/java/com/techelevator/dao/FollowDao.java
@@ -6,10 +6,6 @@ import java.util.List;
 
 public interface FollowDao {
 
-    public int userId;
-
-    public String mbid;
-
     List<Follow> getFollows(int userId);
 
     Follow createFollow(Follow newFollow);
