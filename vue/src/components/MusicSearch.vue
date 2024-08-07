4aa6011c9382ef34f9d83c825767e2dc17e7a254
Madeline Travis <madeline.p.schultz@gmail.com>
2024-08-07 13:25:31 -0400

    Spotify search function working

--- vue/src/components/MusicSearch.vue
+++ vue/src/components/MusicSearch.vue
@@ -2,7 +2,7 @@
     <div>
         <h2>Search an Artist</h2>
         <input type="text" name="userString" id="userString" placeholder="Search for Artists .." v-model="this.userString">
-        <button v-on:click="getInfo" type="button" class="btn btn-outline-primary">Click me to win a prize!!!</button>
+        <button v-on:click="getInfo" type="button" class="btn btn-outline-primary">SEARCH</button>
 
         <div v-show="info != {}">
             <p> {{ info }}</p>
@@ -19,15 +19,26 @@ export default {
     data() {
         return {
             userString: '',
-            info: {}
+            info: [{
+                artistName: '',
+
+            }]
         }
     },
     methods: {
         getInfo() {
+            this.info = [];
             const spotify_token = this.$store.state.spotifyToken;
             console.log(spotify_token);
             MusicSearchService.getTrackInfo(this.userString, spotify_token).then(response => {
-                this.info = response.artists;
+                for(let i = 0; i<response.artists.items.length; i++) {
+                this.info.push(
+                    this.artistId = response.artists.items[i].id,
+                    this.artistName = response.artists.items[i].name,
+                    this.genre = response.artists.items[i].genres,
+                    this.images = response.artists.items[i].images
+                    )
+                }
             })
         }
     }
