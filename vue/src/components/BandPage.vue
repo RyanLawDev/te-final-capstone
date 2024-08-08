d24c75d3a36461bb0c453da697fead4244a3a656
josh brown <jbro463@gmail.com>
2024-08-08 10:26:34 -0400

    pull

--- vue/src/components/BandPage.vue
+++ vue/src/components/BandPage.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="bandContainer">
-    
-    <div id="bandName" type="text"> {{ band.name }} <button
-          v-on:click="toggleFollow(band.id)" v-bind:disabled="this.$store.state.token == ''"
-        > {{ this.$store.state.follows.includes(band.id) ? 'Unfollow' : 'Follow' }}
-        </button></div>
+
+    <div id="bandName" type="text"> {{ band.name }} <button v-on:click="toggleFollow(band.id)"
+        v-bind:disabled="this.$store.state.token == ''"> {{ this.$store.state.follows.includes(band.id) ? 'Unfollow' :
+          'Follow' }}
+      </button></div>
     <ul id="theUL">
-    <div id="bandMembers" v-bind:band=band v-for="member in band.members" v-bind:key="member"> {{ member }} </div>
+      <div id="bandMembers" v-bind:band=band v-for="member in band.members" v-bind:key="member"> {{ member }} </div>
     </ul>
     <div id="bandDescription"> {{ band.description }} {{ band.image }}</div>
-  
+
 
   </div>
 </template>
@@ -17,12 +17,12 @@
 <script>
 export default {
   methods: {
-        toggleFollow(bandId) {
-            this.$store.commit("TOGGLE_FOLLOW", bandId)
-        }
-    },
-    props: ['band'],
-    
+    toggleFollow(bandId) {
+      this.$store.commit("TOGGLE_FOLLOW", bandId)
+    }
+  },
+  props: ['band'],
+
 
 }
 </script>
@@ -32,22 +32,27 @@ export default {
   display: flex;
 
 
-};
+}
+
+;
+
 #bandName {
   font-size: 300px;
-};
-#bandMembers {
-  
+}
 
-};
+;
+
+#bandMembers {
+  font-size: 300px;
+}
+
+;
+
 #theUL {
   display: flex;
-  
-
+  font-size: 300px;
 }
+
 /* #bandDescription {
   
-} */
-
-
-</style>
+} */</style>
