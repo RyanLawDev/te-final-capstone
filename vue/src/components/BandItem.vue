76796b3e991717a3b7bd8a4a22b6292ba145694a
Kari Hollobaugh <karihollobaugh@gmail.com>
2024-08-08 13:17:18 -0400

    pushing band item

--- vue/src/components/BandItem.vue
+++ vue/src/components/BandItem.vue
@@ -1,37 +1,41 @@
 <template>
-    <div class="card mb-3" style="max-width: 80vw;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img v-bind:src="band.images[0].url" alt="Band Image" class="img-fluid rounded-start" >
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{{ band.name }}</h5>
-        <p class="card-text" v-for="genre in band.genres" v-bind:key="genre">{{ genre }}</p>
-        <button
-          v-on:click="toggleFollow(band.id)" v-bind:disabled="this.$store.state.token == ''"
-        > {{ this.$store.state.follows.includes(band.id) ? 'Unfollow' : 'Follow' }}
-        </button>
-        <router-link class="btn btn-primary" v-bind:to="{name : 'bandPage', params: {id : band.id}}">Band Page</router-link>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
+  <router-link href="#" v-bind:to="{name : 'bandPage', params: {id : band.id}}">
+ <div class="card mb-3" style="max-width: 80vw;">
+<div class="row g-0">
+ <div class="col-md-4">
+   <img v-bind:src="band.images[0].url" alt="Band Image" class="img-fluid rounded-start" >
+ </div>
+ <div class="col-md-8">
+   <div class="card-body">
+     <h3>
+         {{band.name}}
+         <p></p>
+     <small class="text-body-secondary" v-for="genre in band.genres" v-bind:key="genre">{{ genre }}&nbsp;|&nbsp;</small>
+     </h3>
+     <a class="card-text" v-on:click.stop v-bind:href="link" target="_blank" v-for="link in band.external_urls" v-bind:key="link">Artist Spotify Page</a>
+     <button
+        v-on:click.stop="toggleFollow(band.id)" v-bind:disabled="this.$store.state.token == ''"
+     > {{ this.$store.state.follows.includes(band.id) ? 'Unfollow' : 'Follow' }}
+     </button>
+   </div>
+ </div>
 </div>
+</div>
+</router-link>
 </template>
-
 <script>
 export default {
-    props: [
-        'band'
-
-    ],
-    methods: {
-        toggleFollow(bandId) {
-            this.$store.commit("TOGGLE_FOLLOW", bandId)
-        }
-    }
+ props: [
+     'band'
+ ],
+ methods: {
+     toggleFollow(bandId) {
+         this.$store.commit("TOGGLE_FOLLOW", bandId)
+     },
+     stopPropagation(event) {
+         event.stopPropagation();
+     }
+ }
 };
 </script>
-
 <style scoped></style>
