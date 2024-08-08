e2d82c787eff7d4bcaef88e8bd62cb67381fb538
Ryan Law <ryanhlaw13@gmail.com>
2024-08-08 15:11:26 -0400

    pull push

--- vue/src/components/SearchBands.vue
+++ vue/src/components/SearchBands.vue
@@ -2,22 +2,13 @@
 
     <div>
 
-        <input type="text" name="bandName" id="bandName" placeholder="Search for bands .." v-model="this.$store.state.bandFilter">
+        <input type="text" name="bandName" id="bandName" placeholder="Search for bands .." v-model="this.$store.state.bandFilter" v-on:keyup="updateArtist">
         
         <div>
             <band-item v-bind:band=band v-for="band in this.artist" v-bind:key="band.id" > </band-item>
             
             
         </div>
-
-    <div>
-      <band-item
-        v-bind:band="band"
-        v-for="band in this.artist"
-        v-bind:key="band.id"
-      >
-      </band-item>
-    </div>
   </div>
 </template>
   
@@ -57,6 +48,31 @@ export default {
     clearSearch() {
       this.bandFilter = "";
     },
+
+    updateArtist() {
+      if (this.$store.state.bandFilter != "") {
+      console.log(this.$store.state.bandFilter);
+      
+      const spotify_token = this.$store.state.spotifyToken;
+      console.log(spotify_token);
+      MusicSearchService.getArtistInfo(
+        this.$store.state.bandFilter,
+        spotify_token
+      ).then((response) => {
+        this.artist = [];
+        for (let i = 0; i < response.artists.items.length; i++) {
+          this.artist.push(
+            response.artists.items[i]
+            // this.artistId = response.artists.items[i].id,
+            // this.artistName = response.artists.items[i].name,
+            // this.genre = response.artists.items[i].genres,
+            // this.images = response.artists.items[i].images,
+            // this.externalUrl = response.artists.items[i].external_urls
+          );
+        }
+      });
+    }
+    }
   },
   created() {
     if (this.$store.state.bandFilter != "") {
