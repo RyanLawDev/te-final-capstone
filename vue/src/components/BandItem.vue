09f97e35edaf38f91c854a4036331ae9d1e4064d
Kari Hollobaugh <karihollobaugh@gmail.com>
2024-08-08 17:46:50 -0400

    getting in sync

--- vue/src/components/BandItem.vue
+++ vue/src/components/BandItem.vue
@@ -3,7 +3,7 @@
  <div id="bandItem" class="card mb-3 shadow p-3 mb-5 bg-body-tertiary rounded" style="max-width: 95vw;">
 <div class="row g-0">
  <div class="col-md-4">
-   <img v-bind:src="band.images.length > 0 ? band.images[0].url : '...'" alt="Band Image" class="img-fluid rounded-start" >
+   <img id="bandImage" v-bind:src="band.images.length > 0 ? band.images[0].url : '...'" alt="Band Image" class="img-fluid rounded-start" >
  </div>
  <div class="col-md-8">
    <div class="card-body">
@@ -12,12 +12,13 @@
          <p></p>
      <small class="text-body-secondary" v-for="genre in band.genres" v-bind:key="genre">{{ genre }}&nbsp;|&nbsp;</small>
      </h3>
-     
-     <button id="spotifyLink" class="btn btn-outline-dark" v-on:click.stop="openLink(link)" v-bind:href="link" target="_blank" v-for="link in band.external_urls" v-bind:key="link">Artist Spotify Page</button>
+    
      <button id="followButton" class="btn btn-outline-dark"
         v-on:click.stop="toggleFollow(band.id)" v-bind:disabled="this.$store.state.token == ''"
      > {{ this.$store.state.follows.includes(band.id) ? 'Unfollow' : 'Follow' }}
      </button>
+     <button id="spotifyLink" class="btn btn-outline-dark" v-on:click.stop="openLink(link)" v-bind:href="link" target="_blank" 
+          v-for="link in band.external_urls" v-bind:key="link">Spotify</button>
    </div>
  </div>
 </div>
