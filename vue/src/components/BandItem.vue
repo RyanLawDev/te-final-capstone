5887b21740d88d486f2d16ee59394d8ed72e5f5f
Ryan Law <ryanhlaw13@gmail.com>
2024-08-09 12:59:26 -0400

    pull

--- vue/src/components/BandItem.vue
+++ vue/src/components/BandItem.vue
@@ -1,34 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <router-link href="#" v-bind:to="{ name: 'bandPage', params: { id: band.id } }">
-    <div id="bandItem" class="card mb-3 shadow p-3 mb-5 bg-body-tertiary rounded" style="max-width: 95vw;">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img id="bandImage" v-bind:src="band.images.length > 0 ? band.images[0].url : '...'" alt="Band Image"
-            class="img-fluid rounded-start">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h3>
-              {{ band.name }}
-              <p></p>
-              <small class="text-body-secondary" v-for="genre in band.genres" v-bind:key="genre">{{ genre
-              }}&nbsp;|&nbsp;</small>
-            </h3>
-
-            <button id="spotifyLink" class="btn btn-outline-dark" v-on:click.stop="openLink(link)" v-bind:href="link"
-              target="_blank" v-for="link in band.external_urls" v-bind:key="link">Artist Spotify Page</button>
-            <button id="followButton" class="btn btn-outline-dark" v-on:click.stop="followBand"
-              v-bind:disabled="this.$store.state.token == ''"> {{'Follow'}}
-            </button>
-
-            <!-- <add-follow v-bind:bandId=bandId v-on:click.stop/> -->
-          </div>
-        </div>
-      </div>
-    </div>
-  </router-link>
-=======
   <router-link href="#" v-bind:to="{name : 'bandPage', params: {id : band.id}}">
  <div id="bandItem" class="card mb-3 shadow p-3 mb-5 bg-body-tertiary rounded" style="max-width: 95vw;">
 <div class="row g-0">
@@ -54,7 +24,6 @@
 </div>
 </div>
 </router-link>
->>>>>>> 5abe40d84d4c5f523481a9502d8c03cdb6f09813
 </template>
 <script>
 //import AddFollow from './AddFollow.vue';
