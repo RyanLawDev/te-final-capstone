bf294fcb09a0fdfe7b9502775aaa24f24611a342
Madeline Travis <madeline.p.schultz@gmail.com>
2024-08-09 16:50:13 -0400

    day 7

--- vue/src/components/BandItem.vue
+++ vue/src/components/BandItem.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <router-link href="#" v-bind:to="{ name: 'bandPage', params: { id: band.id } }">
     <div id="bandItem" class="card mb-3 shadow p-3 mb-5 bg-body-tertiary rounded" style="max-width: 95vw;">
       <div class="row g-0">
@@ -22,54 +21,28 @@
               v-bind:disabled="this.$store.state.token == ''"> {{'Follow'}}
             </button>
 
-            <!-- <add-follow v-bind:bandId=bandId v-on:click.stop/> -->
+            
           </div>
         </div>
       </div>
     </div>
   </router-link>
-=======
-  <router-link href="#" v-bind:to="{name : 'bandPage', params: {id : band.id}}">
- <div id="bandItem" class="card mb-3 shadow p-3 mb-5 bg-body-tertiary rounded" style="max-width: 95vw;">
-<div class="row g-0">
- <div class="col-md-4">
-   <img id="bandImage" v-bind:src="band.images.length > 0 ? band.images[0].url : '...'" alt="Band Image" class="img-fluid rounded-start" >
- </div>
- <div class="col-md-8">
-   <div class="card-body">
-     <h3>
-         {{band.name}}
-         <p></p>
-     <small class="text-body-secondary" v-for="genre in band.genres" v-bind:key="genre">{{ genre }}&nbsp;|&nbsp;</small>
-     </h3>
-      
-     <button id="followButton" class="btn btn-outline-dark"
-        v-on:click.stop="toggleFollow(band.id)" v-bind:disabled="this.$store.state.token == ''"
-     > {{ this.$store.state.follows.includes(band.id) ? 'Unfollow' : 'Follow' }}
-     </button>
-     <button id="spotifyLink" class="btn btn-outline-dark" v-on:click.stop="openLink(link)" v-bind:href="link" target="_blank" 
-          v-for="link in band.external_urls" v-bind:key="link">Spotify</button>
-   </div>
- </div>
-</div>
-</div>
-</router-link>
->>>>>>> 5abe40d84d4c5f523481a9502d8c03cdb6f09813
 </template>
 <script>
-//import AddFollow from './AddFollow.vue';
+
 import BandService from '../services/BandService';
 
 export default {
   props: [
-    'band'
+    'band',
+    //'bandId'
   ],
   components: {
-    //AddFollow
+    
   },
   data() {
         return {
-            bandId: this.band.id
+          bandId: this.band.id
         }
 
     },
@@ -87,8 +60,8 @@ export default {
             
             BandService.createFollow(this.bandId).then((response) => {
 
-            console.log(response.data);
-
+              console.log("Created!");
+              console.log(response.data);
             })
             .catch((error) => {
                 console.log(error)
