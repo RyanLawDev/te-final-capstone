61bd57aac364c4f81830b898146b42e306075f6e
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-08-09 12:48:31 -0400

    prior to pull

--- vue/src/components/BandPage.vue
+++ vue/src/components/BandPage.vue
@@ -26,47 +26,53 @@
       <button id="spotify" class="btn btn-outline-success" v-on:click.stop="openLink(link)" v-bind:href="link"
         target="_blank" v-for="link in artist.external_urls" v-bind:key="link">Spotify</button>
     </div>
-    
+  </div>
 
-  </div>
-  <div class="albumsAccordion" id="accordionExample">
+
+
+
+
+
+  <div class="accordion accordion-flush" id="accordionFlushExample">
   <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        {{album1.name}}
       </button>
     </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">This is the first item's accordion body.</div>
     </div>
   </div>
   <div class="accordion-item">
-    <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
+    <h2 class="accordion-header" id="flush-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+        {{ album2.name }}
       </button>
     </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
+    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
     </div>
   </div>
   <div class="accordion-item">
-    <h2 class="accordion-header" id="headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
+    <h2 class="accordion-header" id="flush-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+        {{ album3.name }}
       </button>
     </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
+    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
     </div>
   </div>
-</div>
+  </div>
+
+
+
+
+
+
+
+
   <!-- <div id="bandDescription"> {{ artistSpotifyUrl }} {{ artistUrl }}</div> -->
 </template>
 
@@ -81,7 +87,10 @@ export default {
       artist: {},
       album: [],
       artistUrl: '',
-      track: []
+      track: [],
+      album1: '',
+      album2: '',
+      album3: '',
     }
   },
   methods: {
@@ -96,10 +105,10 @@ export default {
       let artistId = this.$route.params.id;
       const spotify_token = this.$store.state.spotifyToken;
       MusicSearchService.getTrackByAlbum(albumName, artistId, spotify_token).then(response => {
-        this.album = [];
-        for (let i = 0; i < response.albums.items.length; i++) {
-          this.album.push(
-            response.albums.items[i]
+        this.track = [];
+        for (let i = 0; i < response.tracks.items.length; i++) {
+          this.track.push(
+            response.tracks.items[i]
           );
         }
       }
@@ -119,6 +128,9 @@ export default {
     )
     MusicSearchService.getAlbumByArtist(bandId, spotify_token).then((response) => {
       this.album = [];
+      this.album1 = (response.albums.items[0]);
+      this.album2 = (response.albums.items[1]);
+      this.album3 = (response.albums.items[2]);
       for (let i = 0; i < response.albums.items.length; i++) {
         this.album.push(
           response.albums.items[i]
