45cb4cf1e13374b4c3a8f392a01a39f39958fb43
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-08-09 11:53:08 -0400

    prior to pull

--- vue/src/components/BandPage.vue
+++ vue/src/components/BandPage.vue
@@ -2,13 +2,13 @@
   <div class="bandContainer">
     <div id="bandName" type="text"> {{ artist.name }}
     </div>
-   </div>
+  </div>
 
 
-  
-    <div id="bandImage">
-      <img v-bind:src="artistUrl" alt="Band Image" class="img-fluid rounded-start">
-    </div>
+
+  <div id="bandImage">
+    <img v-bind:src="artistUrl" alt="Band Image" class="img-fluid rounded-start">
+  </div>
 
 
 
@@ -17,24 +17,63 @@
   </div>
 
 
-    <div id="followButton">
-    <button  class="btn btn-outline-dark" v-on:click="toggleFollow(artist.id)"
-        v-bind:disabled="this.$store.state.token == ''"> {{ this.$store.state.follows.includes(artist.id) ? 'Unfollow' :
-          'Follow' }}
-      </button>
+  <div id="followButton">
+    <button class="btn btn-outline-dark" v-on:click="toggleFollow(artist.id)"
+      v-bind:disabled="this.$store.state.token == ''"> {{ this.$store.state.follows.includes(artist.id) ? 'Unfollow' :
+        'Follow' }}
+    </button>
     <div>
       <button id="spotify" class="btn btn-outline-success" v-on:click.stop="openLink(link)" v-bind:href="link"
         target="_blank" v-for="link in artist.external_urls" v-bind:key="link">Spotify</button>
     </div>
-    <div id="albums">
+    
 
-      
-  
-    </div>
- 
   </div>
+  <div id="albumsAccordion">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          {{ album.name[1] }}
+        </button>
+      </h5>
+    </div>
 
-
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <!-- <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Collapsible Group Item #2
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Collapsible Group Item #3
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div> -->
+</div>
   <!-- <div id="bandDescription"> {{ artistSpotifyUrl }} {{ artistUrl }}</div> -->
 </template>
 
@@ -65,13 +104,13 @@ export default {
       const spotify_token = this.$store.state.spotifyToken;
       MusicSearchService.getTrackByAlbum(albumName, artistId, spotify_token).then(response => {
         this.album = [];
-          for (let i = 0; i < response.albums.items.length; i++) {
-            this.album.push(
-              response.albums.items[i]
-            );
-          }
-    }
-    )
+        for (let i = 0; i < response.albums.items.length; i++) {
+          this.album.push(
+            response.albums.items[i]
+          );
+        }
+      }
+      )
     }
   },
   created() {
@@ -86,13 +125,13 @@ export default {
     }
     )
     MusicSearchService.getAlbumByArtist(bandId, spotify_token).then((response) => {
-          this.album = [];
-          for (let i = 0; i < response.albums.items.length; i++) {
-            this.album.push(
-              response.albums.items[i]
-            );
-          }
-        })
+      this.album = [];
+      for (let i = 0; i < response.albums.items.length; i++) {
+        this.album.push(
+          response.albums.items[i]
+        );
+      }
+    })
   },
 
   props: [
@@ -103,13 +142,13 @@ export default {
 
 <style scoped>
 .bandContainer {
-  display:flex;
-  justify-content:center;
+  display: flex;
+  justify-content: center;
   align-items: center;
 }
 
-#spotify{
-  display:block;
+#spotify {
+  display: block;
   margin-left: auto;
   margin-right: auto;
   width: 30%;
@@ -118,24 +157,27 @@ export default {
 #bandName {
   display: flex;
   font-size: 3000;
-  font-family:monospace;
-  color:black;
+  font-family: monospace;
+  color: black;
   justify-content: baseline;
   align-content: end;
-};
+}
 
-#followButton{
-  display:flex;
-  justify-content:center;
+;
+
+#followButton {
+  display: flex;
+  justify-content: center;
   align-items: center;
 }
 
-#bandImage{
-  display:block;
+#bandImage {
+  display: block;
   margin-left: auto;
   margin-right: auto;
   width: 30%;
 }
+
 ;
 
 #followButton {
@@ -144,5 +186,4 @@ export default {
   border-radius: 10px;
   box-shadow: 0px 0px 2px 2px rgb(0, 0, 0);
 
-}
-</style>
+}</style>
