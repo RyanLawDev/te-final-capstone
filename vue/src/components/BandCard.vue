74c1be588301e01241054dcb2519e9a1605d113e
Kari Hollobaugh <karihollobaugh@gmail.com>
2024-08-12 11:40:27 -0400

    eric said to pull

--- vue/src/components/BandCard.vue
+++ vue/src/components/BandCard.vue
@@ -2,9 +2,7 @@
     <div class="card">
         <img v-bind:src="artistUrl" class="card-img" alt="Artist Image">
         <div class="card-body">
-            <button id="follow-button" v-on:click="toggleFollow(band.id)" v-bind:disabled="isDisabled">
-                {{ this.$store.state.follows.includes(band.id) ? 'Unfollow' : 'Follow' }}
-            </button>
+            
             <h5 class="card-title">{{ artist.name }}</h5>
             <div class="genres">
                 <small v-for="genre in artist.genres" v-bind:key="genre" class="genre-chip">
@@ -91,7 +89,7 @@ export default {
 }
 
 .card-title {
-    font-size: 1.25rem; /* Font size for title */
+    font-size: 1.4rem; /* Font size for title */
     margin: 12px 0; /* Margin around title */
 }
 
@@ -129,7 +127,7 @@ export default {
 .band-page-button:hover {
     background-color: black;
 }
-#follow-button {
+/* #follow-button {
     display: inline-block;
     margin-top: 12px;
     padding: 8px 16px;
@@ -140,5 +138,19 @@ export default {
     text-decoration: none;
     box-shadow: 0px 0px 2px 2px rgb(0, 0, 0);
 }
+#follow-button:hover {
+    background-color: black;
+    color: #fff;
+} */
 
+
+
+.card {
+    padding: 20px;
+    margin: 20px;
+    border-radius: 10px;
+    background-color:#fcf5e5;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3); /* Initial black glow */
+    /* animation: pulse 2s infinite; Pulsing effect */
+}
 </style>
