a4c64ec022db2d2145c0b83df36b979ac709b962
Ryan Law <ryanhlaw13@gmail.com>
2024-08-15 08:37:15 -0400

    pull

--- java/database/data.sql
+++ java/database/data.sql
@@ -6,8 +6,10 @@ INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULi
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 INSERT INTO users (username,password_hash,role) VALUES ('madeline','$2a$10$QGyZ.Uv5/QgOkbZg3nko0emVckRP4qmUzx/UQ4bHTP9vs2P6bM1G2','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('andy','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 INSERT INTO adminuser_band (band_id, user_id)  VALUES ('0fgYKF9Avljex0L9Wt5b8Z', 3);
+INSERT INTO adminuser_band (band_id, user_id) VALUES ('41lcf5k3PkUdxupYLkcjCd', 4);
 
 INSERT INTO notifications (spotify_band_id, message) VALUES ('7muzHifhMdnfN1xncRLOqk', 'Calling all fans! Check out our new tour dates.');
 INSERT INTO notifications (spotify_band_id, message) VALUES ('7muzHifhMdnfN1xncRLOqk', 'Calling all fans! Check out our new tour dates.');
